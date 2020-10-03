51f76dc278729f369fc95b853052c1857376e4f3
by <906200931@qq.com>
2020-10-03 15:14:47 +0800

    简易选课系统

---
+++ collection_demo/src/com/collection/Course.java
@@ -0,0 +1,20 @@
+package com.collection;
+
+//øŒ≥Ã¿‡
+public class Course {
+	
+	public String id;
+	
+	public String name;
+	
+	public Course(String id, String name) {
+		
+		this.id = id;
+		
+		this.name = name;
+	}
+	
+	public Course() {
+		
+	}
+}
